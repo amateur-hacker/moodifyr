ab3f191a266b5c390e903218abac8d9c714782e5
amateur-hacker <amateurhacker2004@gmail.com>
2025-10-27 14:45:28 +0530

    made changes

--- src/app/_components/play-header.tsx
+++ src/app/_components/play-header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Dot, ListMusic, Pause, Play, Shuffle } from "lucide-react";
+import { Dot, EllipsisIcon, ListMusic, Shuffle } from "lucide-react";
 import { useState } from "react";
 import { useSongPlayer } from "@/app/_context/song-player-context";
 import type { SongWithUniqueIdSchema } from "@/app/_types";
@@ -12,7 +12,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Spinner } from "@/components/ui/spinner";
 import { Typography } from "@/components/ui/typography";
 import { cn } from "@/lib/utils";
 
@@ -38,10 +37,7 @@ const PlayHeader = ({
     setIsPlayerFullScreen,
     recentSongIdsRef,
     playerRef,
-    isPlaying,
-    setIsPlaying,
     setMode,
-    mode,
     lastActionRef,
   } = useSongPlayer();
 
@@ -76,101 +72,42 @@ const PlayHeader = ({
     }
 
     setIsLoading(false);
-    setIsPlaying(true);
-  };
-
-  const handlePause = () => {
-    setIsPlaying(false);
   };
 
   return (
-    <div
-      className={cn(
-        "flex flex-col gap-2 justify-center items-center",
-        className,
-      )}
-    >
+    <div className={cn("flex gap-2 justify-center items-center", className)}>
       <div className="flex items-center gap-0.5 text-muted-foreground">
         <Typography variant="body">{totalSongs} songs</Typography>
         <Dot />
         <Typography variant="body">{totalTime}</Typography>
       </div>
 
-      {isPlaying ? (
-        <div className="relative">
-          <Button
-            variant="outline"
-            size="icon"
-            className="rounded-full size-14 cursor-pointer"
-            disabled={isLoading}
-            title="Pause"
-            onClick={handlePause}
-          >
-            {isLoading ? (
-              <Spinner className="size-7" />
-            ) : (
-              <Pause className="size-7" aria-hidden />
-            )}
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon" variant="ghost">
+            <EllipsisIcon size={16} />
           </Button>
+        </DropdownMenuTrigger>
 
-          {mode && (
-            <div className="absolute bottom-1 right-1 bg-background rounded-full p-1 shadow-sm">
-              {mode === "shuffle" ? (
-                <Shuffle className="size-3 text-muted-foreground" />
-              ) : (
-                <ListMusic className="size-3 text-muted-foreground" />
-              )}
-            </div>
-          )}
-        </div>
-      ) : (
-        <DropdownMenu open={open} onOpenChange={setOpen}>
-          <DropdownMenuTrigger asChild>
-            <div className="relative">
-              <Button
-                variant="outline"
-                size="icon"
-                className="rounded-full size-14 cursor-pointer"
-                disabled={isLoading}
-                title="Open play options menu"
-              >
-                {isLoading ? (
-                  <Spinner className="size-7" />
-                ) : (
-                  <Play className="size-7" aria-hidden />
-                )}
-              </Button>
-
-              {mode && (
-                <div className="absolute bottom-1 right-1 bg-background rounded-full p-1 shadow-sm">
-                  {mode === "shuffle" ? (
-                    <Shuffle className="size-3 text-muted-foreground" />
-                  ) : (
-                    <ListMusic className="size-3 text-muted-foreground" />
-                  )}
-                </div>
-              )}
-            </div>
-          </DropdownMenuTrigger>
-
-          <DropdownMenuContent align="center" className="w-40">
-            <DropdownMenuItem
-              onClick={() => handlePlay("normal")}
-              className="cursor-pointer flex items-center gap-2"
-            >
-              <ListMusic className="size-4" />
-              Normal Play
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() => handlePlay("shuffle")}
-              className="cursor-pointer flex items-center gap-2"
-            >
-              <Shuffle className="size-4" />
-              Shuffle Play
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )}
+        <DropdownMenuContent align="center" className="w-40">
+          <DropdownMenuItem
+            onClick={() => handlePlay("normal")}
+            disabled={isLoading}
+            className="cursor-pointer flex items-center gap-2"
+          >
+            <ListMusic className="size-4" />
+            Normal Play
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => handlePlay("shuffle")}
+            disabled={isLoading}
+            className="cursor-pointer flex items-center gap-2"
+          >
+            <Shuffle className="size-4" />
+            Shuffle Play
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
